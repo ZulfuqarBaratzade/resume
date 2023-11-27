ffe35633f99db4b52cb53cefabe327fece141736
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2023-11-27 16:54:44 +0400

    use template

--- resume/settings.py
+++ resume/settings.py
@@ -122,16 +122,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT=BASE_DIR /'staticfiles'
-STATIC_FILES_DIR = [
+
+STATIC_URL = '/static/'
+STATIC_ROOT= BASE_DIR / 'staticfiles'
+
+MEDIA_URL= '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-#MEDIA 
 
-MEDIA_URL='/media/'
-MEDIA_ROOT=BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
