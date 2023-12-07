91f7ac2197013b3fd0f01bb94d3132e2a4d50d5d
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2023-12-07 20:05:36 +0400

    feat: create general admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from core.models import *
 # Register your models here.
+
+
+@admin.register(GeneralSetting)
+class GeneralSettingsAdmin(admin.ModelAdmin):
+    list_display = ['id','name','description','parameter','created_date','updated_date']
+    search_fields = ['name','description','parameter']
+    list_editable=['name','description']
+
+    class Meta:
+        model = GeneralSetting
+
--- core/models.py
+++ core/models.py
@@ -3,30 +3,39 @@ from django.db import models
 # Create your models here.
 
 
-class GeneralSettings(models.Model):
+class GeneralSetting(models.Model):
     name = models.CharField(
         default='',
         max_length=254,
-        blank=True
+        blank=True,
+        verbose_name= 'name',
 
 
     )
     description=models.CharField(
         default='',
         max_length=254,
-        blank=True
+        blank=True,
+        verbose_name= 'description',
     )
     parameter = models.CharField(
         default='',
         max_length=254,
-        blank=True
+        blank=True,
+        verbose_name= 'parameter',
     )
     updated_date = models.DateTimeField(
         blank=True,
         auto_now=True,
+        
     )
     created_date = models.DateTimeField(
         blank=True,
         auto_now_add=True,
     )
-    
+    def __str__(self):
+        return f'General Setting {self.name}'
+    class Meta:
+        verbose_name = "General Setting"
+        verbose_name_plural = "General Setting"
+        ordering = ('name',)
