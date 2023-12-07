3d9d739983a50252d9bf5d5ffa83d9a3a0fc9e93
zulfuqarbaratzade <zulfuqar.baratzade@gmail.com>
2023-12-07 16:27:35 +0400

    feat: create general model

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import home,contact
+from .views import home
 
 
 urlpatterns = [
     path('',home,name='home'),
-    path('contact/',contact,name='contact')
+    
 
 ]
--- core/views.py
+++ core/views.py
@@ -6,5 +6,3 @@ from django.shortcuts import render
 def home(request):
     return render(request,'index.html')
 
-def contact(request):
-    return render(request,'contact.html')
