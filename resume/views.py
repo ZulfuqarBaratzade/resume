19e8f4bfc07559c98b41f0ce76d0ef710efef265
zulfuqar.baratzade <zulfuqar.baratzade@gmail.com>
2024-03-22 00:44:32 +0400

    feat:create site_title

--- resume/views.py
+++ resume/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from resume.models import GeneralSetting
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    site_title=GeneralSetting.objects.get(name='site_title').parameter
+    context={
+        "site_title":site_title,
+    }
+    return render(request,"index.html",context)
