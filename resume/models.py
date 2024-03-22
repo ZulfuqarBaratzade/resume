bfec40687f700f14fc58a0266b2578f01e67de76
zulfuqar.baratzade <zulfuqar.baratzade@gmail.com>
2024-03-22 19:40:58 +0400

    feat: create Image Model

--- resume/models.py
+++ resume/models.py
@@ -37,3 +37,41 @@ class GeneralSetting(models.Model):
         verbose_name="General Setting"
         verbose_name_plural="General Settings"
         ordering = ("name", "parameter")
+
+
+
+class ImageSetting(models.Model):
+    name=models.CharField(
+        default="",
+        max_length=254,
+        blank=True,
+        verbose_name="name"
+    )
+    description=models.CharField(
+        default="",
+        max_length=254,
+        blank=True,
+        verbose_name="description"
+    )
+    image=models.ImageField(
+        default='',
+        verbose_name='Image',
+        help_text="",
+        blank=True,
+        upload_to='image/',
+    )
+    updated_date=models.DateTimeField(
+        blank=True,
+        auto_now=True,
+        verbose_name="updated_data"
+    )
+    created_date=models.DateField(
+        blank=True,
+        auto_now_add=True,
+        verbose_name="created_data")
+    def __str__(self):
+        return f"Image setting {self.name}"
+    class Meta:
+        verbose_name="Image Setting"
+        verbose_name_plural="Image Settings"
+        ordering="image"
--- resume/views.py
+++ resume/views.py
@@ -4,7 +4,9 @@ from resume.models import GeneralSetting
 # Create your views here.
 def index(request):
     site_title=GeneralSetting.objects.get(name='site_title').parameter
+    home_banner=GeneralSetting.objects.get(name='home_banner').parameter
     context={
         "site_title":site_title,
+        "home_banner":home_banner,
     }
     return render(request,"index.html",context)
