97898ae3b183ef1e6ca17143761da8c3bebaf400
zulfuqar.baratzade <zulfuqar.baratzade@gmail.com>
2024-03-26 20:27:56 +0400

    bug:fixed

--- resume/views.py
+++ resume/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponseRedirect
 from resume.models import GeneralSetting,ImageSetting,TextSetting,Contact
 from django.contrib import messages
-
+from resume.forms import ContactForm
 # Create your views here.
 def index(request):
     site_title=GeneralSetting.objects.get(name='site_title').parameter
@@ -15,13 +15,30 @@ def index(request):
         'profile':profile,
         'text':text,
     }
+    url=request.META.get("HTTP_REFERER")
+    if request.method=="POST":
+        form=ContactForm(request.POST)
+        if form.is_valid():
+            message=form.save(commit=False)
+            message.save()
+            
+            return redirect("index")
+        else:
+            context['form'] = ContactForm()
+        messages.success(request,"Thanks")
     return render(request,"index.html",context)
 
-def contact_message(request):
-    if request.method=="POST":
-        contact=Contact()
-        email=request.POST.get("name")
-        text=request.POST.get("text")
-        contact.email=email
-        contact.text=text
+# def contact_message(request):
+#     context=dict()
+#     url=request.META.get("HTTP_REFERER")
+#     if request.method=="POST":
+#         form=ContactForm(request.POST)
+#         if form.is_valid():
+#             message=form.save(commit=False)
+#             message.save()
+#             return HttpResponseRedirect(url)
+#         else:
+#             context['form'] = ContactForm()
+        
+
         
